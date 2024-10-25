179403626e011e1003e1446a336fe52ee026dd45
evlasnoraa <85684345+evlasnoraa@users.noreply.github.com>
2024-10-25 15:04:35 +0100

    Updated directory

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ themeToggle.addEventListener('click', () => {
 
   // Change the icon based on the current mode
   if (document.body.classList.contains('dark-mode')) {
-    themeToggle.src = 'Images/sun-icon.svg';  // Switch to sun icon
+    themeToggle.src = '/Images/sun-icon.svg';  // Switch to sun icon
     themeToggle.alt = 'Switch to Light Mode';
     localStorage.setItem('theme', 'dark'); // Store dark mode preference
   } else {
-    themeToggle.src = 'Images/moon-icon.svg'; // Switch to moon icon
+    themeToggle.src = '/Images/moon-icon.svg'; // Switch to moon icon
     themeToggle.alt = 'Switch to Dark Mode';
     localStorage.setItem('theme', 'light'); // Store light mode preference
   }
@@ -21,10 +21,10 @@ window.addEventListener('DOMContentLoaded', () => {
   const storedTheme = localStorage.getItem('theme');
   if (storedTheme === 'dark') {
     document.body.classList.add('dark-mode');
-    themeToggle.src = 'Images/sun-icon.svg'; // Show sun icon in dark mode
+    themeToggle.src = '/Images/sun-icon.svg'; // Show sun icon in dark mode
     themeToggle.alt = 'Switch to Light Mode';
   } else {
-    themeToggle.src = 'Images/moon-icon.svg'; // Show moon icon in light mode
+    themeToggle.src = '/Images/moon-icon.svg'; // Show moon icon in light mode
     themeToggle.alt = 'Switch to Dark Mode';
   }
 });
